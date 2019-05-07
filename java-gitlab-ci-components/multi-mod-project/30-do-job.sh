e8f43d8511a7f010d45f652200e49ce0643d74d6
夏禹 <xiayu@wanshare.com>
2019-05-07 13:26:08 +0800

    add do job

--- java-gitlab-ci-components/multi-mod-project/30-do-job.sh
+++ java-gitlab-ci-components/multi-mod-project/30-do-job.sh
@@ -4,6 +4,21 @@
 # 运行环境: zgwldrc/kubectl:v1.11.7
 # 说明: 运行一个image为$REGISTRY/$REGISTRY_NAMESPACE/$APP_NAME:${CI_COMMIT_SHA:0:8}的任务镜像
 # 该任务镜像要求挂载一个名为${APP_NAME}-env的configMap，并从中获取环境变量，从而正确的运行
+# Env Var Must
+# APP_NAME
+# CONTEXT
+# KUBECONFIG_CONTENT
+# REGISTRY
+# REGISTRY_NAMESPACE
+# CI_COMMIT_SHA
+ENV_CHECK_LIST='
+APP_NAME
+CONTEXT
+KUBECONFIG_CONTENT
+REGISTRY
+REGISTRY_NAMESPACE
+CI_COMMIT_SHA
+'
 function check_env(){
   local r
   for i do
@@ -14,7 +29,15 @@ function check_env(){
     fi
   done
 }
-check_env APP_NAME REGISTRY REGISTRY_NAMESPACE CI_COMMIT_SHA
+function _init_env(){
+    mkdir -p $HOME/.kube/
+    echo -e "$KUBECONFIG_CONTENT" > $HOME/.kube/config
+    kubectl config use-context $CONTEXT
+}
+
+set -e
+check_env $ENV_CHECK_LIST
+_init_env
 
 if kubectl get configmaps ${APP_NAME}-env &> /dev/null;then
     kubectl run --rm $APP_NAME --generator=run-pod/v1 --restart=Never --attach \
