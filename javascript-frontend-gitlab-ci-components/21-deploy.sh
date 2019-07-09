5e51b93fd2a0f5f0831699ca57d70b9709615766
zgwldrc <zgwldrc@163.com>
2019-07-09 10:29:25 +0800

    1

--- javascript-frontend-gitlab-ci-components/21-deploy.sh
+++ javascript-frontend-gitlab-ci-components/21-deploy.sh
@@ -1,26 +1,35 @@
 #!/bin/sh
 # 作者: 夏禹
 # 邮箱: zgwldrc@163.com
-# 运行环境: zgwldrc/kubectl:v1.11.7
+# 运行环境: zgwldrc/kubectl:v1.11.7-helm
 # K8S Deployment Script For 前端主平台 Used in Gitlab-CI
 
 # Env Var Must
+# APP_NAME: 应用程序名称，等同于Chart名
+# 
+# CHART_PASSWD: Chart仓库密码
+# 
+# CHART_REPO: Chart仓库地址 https://repo.chart.com/
+# CHART_USER: Chart仓库用户名
+# CI_COMMIT_SHA: GITLAB 内置变量, 提交hash
 # CONTEXT
 # KUBECONFIG_CONTENT
 # REGISTRY
 # REGISTRY_NAMESPACE
-# APP_NAME
-# CI_COMMIT_SHA
-
+# RELEASE_NAME
 
 # 必要的环境变量
 ENV_CHECK_LIST='
+APP_NAME
+CHART_PASSWD
+CHART_REPO
+CHART_USER
+CI_COMMIT_SHA
 CONTEXT
 KUBECONFIG_CONTENT
 REGISTRY
 REGISTRY_NAMESPACE
-APP_NAME
-CI_COMMIT_SHA
+RELEASE_NAME
 '
 
 function check_env(){
@@ -43,4 +52,9 @@ function _init_env(){
 check_env $ENV_CHECK_LIST
 _init_env
 
-kubectl set image deploy $APP_NAME $APP_NAME=$REGISTRY/$REGISTRY_NAMESPACE/${APP_NAME}-${CONTEXT}:${CI_COMMIT_SHA:0:8} || true
+helm upgrade $RELEASE_NAME $APP_NAME --reuse-values \
+  --repo $CHART_REPO --username $CHART_USER --password $CHART_PASSWD \
+  --set image.repository=$REGISTRY/$REGISTRY_NAMESPACE/$APP_NAME-${CONTEXT} \
+  --set image.tag=${CI_COMMIT_SHA:0:8} --wait
+
+#kubectl set image deploy $APP_NAME $APP_NAME=$REGISTRY/$REGISTRY_NAMESPACE/${APP_NAME}-${CONTEXT}:${CI_COMMIT_SHA:0:8} || true
